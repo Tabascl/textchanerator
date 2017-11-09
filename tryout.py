d94967e92aaf8e3e2833ff1f44ea750f2206051a
yRefl3x <simonwilde93@gmail.com>
2017-11-09 18:33:53 +0100

    Fix the fucking error finally

--- tryout.py
+++ tryout.py
@@ -6,7 +6,7 @@ data = dataset.DataSet(text)
 char_size = data.char_size
 
 num_layers = 2
-hidden_size = 512
+hidden_size = 256
 batch_size = 512
 len_per_section = 50
 
@@ -23,14 +23,16 @@ biases = {
 }
 
 cells = []
-for i in range(num_layers):
+for _ in range(num_layers):
     cells.append(tf.nn.rnn_cell.BasicLSTMCell(hidden_size))
 
-rnn_cell = tf.nn.rnn_cell.MultiRNNCell(cells)
+rnn_cell = tf.nn.rnn_cell.MultiRNNCell(cells, state_is_tuple=True)
 x = tf.unstack(X, len_per_section, 1)
-outputs, states = tf.nn.static_rnn(rnn_cell, x, dtype=tf.float32)
+outputs, states = tf.nn.dynamic_rnn(rnn_cell, X, dtype=tf.float32)
 
 logits = tf.matmul(outputs[-1], weights['out']) + biases['out']
+
+tf.global_variables_initializer().run()
 
 print(sess.run(outputs, feed_dict={X: data.next_batch(batch_size, len_per_section)[0]}))
 sess.close()
